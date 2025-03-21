e94e91c2f42a2d2591c8db9cb0133bcf2fe7210a
Dejnyyy <danslaby@email.cz>
2025-03-21 09:41:46 -0400

    taka

--- src/pages/components/Roulette/index.tsx
+++ src/pages/components/Roulette/index.tsx
@@ -340,9 +340,9 @@ export default function RouletteWheel() {
             setSecretError("");
             setSecretInput("");
           }}
-          className="absolute top-16 text-black font-semiold right-4 px-4 py-2 bg-white rounded-md text-sm hover:bg-gray-400 transition-colors"
+          className="absolute top-16 text-black font-semibold right-4 px-4 py-2 bg-white rounded-md text-sm hover:bg-gray-400 transition-colors"
         >
-          Codes:
+          CODES
         </button>
       )}
 
@@ -471,7 +471,7 @@ export default function RouletteWheel() {
               </button>
               <button
                 onClick={handleSecretSubmit}
-                className="px-4 py-2 bg-white text-black font-semibold rounded-md hover:bg-gray-400"
+                className="px-4 py-2 bg-white border-gray-200 text-black font-semibold rounded-md hover:bg-gray-200"
               >
                 $$$$$
               </button>
